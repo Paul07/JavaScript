e3999393ce1e1fb13104edd6842c19339e03d667
Paul Brandenburger <paulbrandenburger1@gmail.com>
2022-12-15 13:26:56 -0500

    Update main.js
    added comments

--- Project2_functions/js/main.js
+++ Project2_functions/js/main.js
@@ -1,5 +1,5 @@
-function my_first_function() {
-	var str = "This is the button text!";
-	str += " but so is this!";
+function my_first_function() {//defined the function
+	var str = "This is the button text!";//assigned the str variable
+	str += " but so is this!";//concatenated the str variable
 	document.getElementById("Button_Text").innerHTML = str;
-}
+}//I think it worked
