6932287fba45924b2e1573ce598e2fe3f2ef093a
Paul Brandenburger <paulbrandenburger1@gmail.com>
2022-12-15 12:58:12 -0500

    Add files via upload
    Added first JS work

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,15 @@
+var y="Fingers crossed"//assigning a variable
+var x="Wake up sleepy head!";//assigning a variable
+var z=" millions" + " and millions"//assigning a variable
+var me=" me", her=" her", you=" you", us=" us";//assigning a series of variable
+var sent1=" this is the beginning of the string";//assigning a variable
+var sent2=" and this is the end of the string."//assigning a variable
+window.alert (y);//writing "y" to the display
+document.write (x);//writing "x" to the display
+document.write ("\"It\'s the end of the world as we know it,"
++ " and I feel fine\""
++ "-REM");//writing a concatenated string to the display
+document.write (z);//writing "z" to the display
+document.write (you);//writing "you" to the display
+document.write (5*5);//writing the solution of an expression to the display
+document.write (sent1 + sent2);
