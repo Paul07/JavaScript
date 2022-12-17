88a5df465f28a0d89df4be76538759a85c86aa72
Paul Brandenburger <paulbrandenburger1@gmail.com>
2022-12-17 10:34:46 -0500

    Add files via upload
    comparison javascript exercises

---
+++ Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,72 @@
+document.write(!(2<5));
+
+document.write(!(2>5));
+
+document.write(2<5 || 1>23);
+
+document.write(2>5 || 5<2);
+
+document.write(typeof 7.5);
+
+document.write(2<5 && 5>2);
+
+document.write(2<5 && 5<2);
+
+document.write(" seven" + 9);
+
+document.write(25e500);
+
+document.write(-25e500);
+
+document.write(2==5);
+
+document.write(5==5);
+
+
+function not() {
+	document.getElementById("not").innerHTML = 0/0;
+}
+
+function no() {
+	document.getElementById("no").innerHTML = isNaN("words");
+}
+
+function yes() {
+	document.getElementById("yes").innerHTML = isNaN(25);
+}
+
+function troo() {
+	document.getElementById("true").innerHTML = (2 < 5);
+}
+
+function phalse() {
+	document.getElementById("false").innerHTML = (2 > 5);
+}
+
+console.log(2+5);
+
+console.log(2>5);
+
+plant="five";
+car="five";
+document.write(plant===car);
+
+plants=2;
+cars="five";
+document.write(plants===cars);
+
+tree=2;
+5;
+document.write(tree===5);
+
+dog="good";
+age=9;
+document.write(dog===age);
+
+
+
+
+
+
+
+
